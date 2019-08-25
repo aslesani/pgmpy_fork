b7addf986d33b665031786df50610148026ebf62
aslesani <aslesani@gmail.com>
2019-08-26 02:07:45 +0430

    delete extra empty lines from openshs generated dataset files

---
+++ src/default_test/clean_openshs_files.py
@@ -0,0 +1,28 @@
+'''
+Created on Aug 24, 2019
+
+@author: Adele
+'''
+import csv
+import os
+
+
+def remove_blank_lines(in_filename):
+        
+    temp_filename = r"E:\Lessons_tutorials\Behavioural user profile articles\openSHS\openshs-master-new\app\datasets\dataset2.csv"
+
+    with open(in_filename) as in_file:
+        with open(temp_filename, 'w') as out_file:
+            writer = csv.writer(out_file)
+            for row in in_file.readlines():
+                if row.strip() != '':
+                    out_file.write(row)
+    
+    os.remove(in_filename)	
+    os.rename(temp_filename, in_filename)	
+					
+					
+if __name__ == "__main__":
+    in_file = r"E:\Lessons_tutorials\Behavioural user profile articles\openSHS\openshs-master-new\app\datasets\dataset.csv"
+
+    remove_blank_lines(in_file)
