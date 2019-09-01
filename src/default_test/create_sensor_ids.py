511fa57bf4891d4d2a1e5179db93ea5906854b2c
aslesani <aslesani@gmail.com>
2019-09-02 00:45:50 +0430

    prepare to call for simulation data

--- src/default_test/create_sensor_ids.py
+++ src/default_test/create_sensor_ids.py
@@ -36,10 +36,11 @@ if __name__ == "__main__":
     Tulum2009 = ['M001', 'M002', 'M003', 'M004', 'M005', 'M006', 'M007', 'M008', 'M009', 'M010', 'M011', 'M012', 'M013', 'M014', 'M015', 'M016']
     Tulum2010 = ['M001', 'M002', 'M003', 'M004', 'M005', 'M006', 'M007', 'M008', 'M009', 'M010', 'M011', 'M012', 'M013', 'M014', 'M015', 'M016', 'M017', 'M018', 'M019', 'M020', 'M021', 'M022', 'M023', 'M024', 'M025', 'M026', 'M027', 'M028', 'M029', 'M030', 'M031']
     twor2010 = ['D001', 'D002', 'D003', 'D004', 'D005', 'D006', 'D007', 'D008', 'D009', 'D010', 'D011', 'D012', 'D013', 'D014', 'D015', 'I006', 'I010', 'I011', 'I012', 'M001', 'M002', 'M003', 'M004', 'M005', 'M006', 'M007', 'M008', 'M009', 'M010', 'M011', 'M012', 'M013', 'M014', 'M015', 'M016', 'M017', 'M018', 'M019', 'M020', 'M021', 'M022', 'M023', 'M024', 'M025', 'M026', 'M027', 'M028', 'M029', 'M030', 'M031', 'M032', 'M033', 'M034', 'M035', 'M036', 'M037', 'M038', 'M039', 'M040', 'M041', 'M042', 'M043', 'M044', 'M045', 'M046', 'M047', 'M048', 'M049', 'M050', 'M051']
-    
+    OpenSHS = ["wardrobe","tv","oven","officeLight","officeDoorLock","officeDoor","officeCarp","office","mainDoorLock","mainDoor","livingLight","livingCarp","kitchenLight","kitchenDoorLock","kitchenDoor","kitchenCarp","hallwayLight","fridge","couch","bedroomLight","bedroomDoorLock","bedroomDoor","bedroomCarp","bedTableLamp","bed","bathroomLight","bathroomDoorLock","bathroomDoor","bathroomCarp"]
     Test = r"E:\pgmpy\Test\annotated"
 
-    list_of_Test_sensors, number_of_allowed_samples, list_of_works = get_list_of_allowed_sensors_and_works_in_dataset(Test)
-    print(list_of_Test_sensors)
-    a = create_header_string(list_of_Test_sensors)
+    #list_of_Test_sensors, number_of_allowed_samples, list_of_works = get_list_of_allowed_sensors_and_works_in_dataset(Test)
+    #print(list_of_Test_sensors)
+    #a = create_header_string(list_of_Test_sensors)
+    a = create_header_string(OpenSHS)
     print(a)
